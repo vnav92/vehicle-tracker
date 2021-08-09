4cc0831ac11b55950a692721e77ababe3d93b051
mateusz.jankowski <vortexflameout@gmail.com>
2021-08-09 09:35:41 +0200

    fix(server): handle cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET"]
+  }});
 
 const { Ufo } = require("./classes/ufo");
 const { initialData } = require("./consts/initial-data");
